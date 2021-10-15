e784f7e764dba05ae7ff7e4c3ef1be17dfb56dcb
Kellen Wiltshire <kellenwiltshire@outlook.com>
2021-10-15 08:34:57 -0400

    beggining homepage

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
+import Hero from '../components/home/Hero';
+
 export default function Home() {
-	return (
-		<div className='flex flex-col items-center justify-center min-h-screen py-2'>
-			Women On Par
-		</div>
-	);
+	return <Hero />;
 }
