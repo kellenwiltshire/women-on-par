ac1bceedef8fefffc661eb26a78bcf4b8090575f
Kellen Wiltshire <kellenwiltshire@outlook.com>
2021-10-24 12:09:01 -0400

    Navbar updates

--- components/layout/Navbar.jsx
+++ components/layout/Navbar.jsx
@@ -40,13 +40,7 @@ export default function Navbar() {
 										href='#'
 										className='border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
 									>
-										Team
-									</a>
-									<a
-										href='#'
-										className='border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
-									>
-										Projects
+										News
 									</a>
 									<a
 										href='#'
@@ -88,7 +82,20 @@ export default function Navbar() {
 															'block px-4 py-2 text-sm text-gray-700',
 														)}
 													>
-														Your Profile
+														Your Dashboard
+													</a>
+												)}
+											</Menu.Item>
+											<Menu.Item>
+												{({ active }) => (
+													<a
+														href='#'
+														className={classNames(
+															active ? 'bg-gray-100' : '',
+															'block px-4 py-2 text-sm text-gray-700',
+														)}
+													>
+														Scores
 													</a>
 												)}
 											</Menu.Item>
@@ -114,7 +121,7 @@ export default function Navbar() {
 															'block px-4 py-2 text-sm text-gray-700',
 														)}
 													>
-														Sign out
+														Sign Out
 													</a>
 												)}
 											</Menu.Item>
