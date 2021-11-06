baa7974f7f50dd37a7720a20009ba6ce7cb76355
Kellen Wiltshire <kellenwiltshire@outlook.com>
2021-11-06 13:58:38 -0400

    Edit Score Modal Added

--- components/Modals/EditScore.jsx
+++ components/Modals/EditScore.jsx
@@ -203,14 +203,16 @@ export default function EditScore({ open, setOpen, lastScheduledRound, user }) {
 											>
 												Save
 											</button>
-											<button
-												type='reset'
-												className='inline-flex items-center px-6 py-3 border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-											>
-												Reset
-											</button>
 										</div>
 									</form>
+									<div className='flex w-full justify-end'>
+										<button
+											onClick={() => setOpen(false)}
+											className='inline-flex items-center px-6 py-3 border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+										>
+											Close
+										</button>
+									</div>
 								</div>
 							</div>
 						</div>
