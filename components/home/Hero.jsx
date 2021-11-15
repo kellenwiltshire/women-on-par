438b84d944c547e3df320c0e63cf68ca2604af27
Kellen Wiltshire <kellenwiltshire@outlook.com>
2021-11-15 09:04:24 -0500

    Added Mission Statement

--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -7,8 +7,18 @@ export default function Hero() {
 			<div className='mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-48 lg:text-left'>
 				<div className='px-4 lg:w-1/2 sm:px-8 xl:pr-16'>
 					<h1 className='text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl'>
-						<span className='block xl:inline'>Women on par</span>
+						<span className='block xl:inline'>Women on Par</span>
 					</h1>
+					<p className='mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl'>
+						Since 1990 Women On Par has offered a fun and welcoming environment
+						for women to enjoy playing golf. We value fair play and provide an
+						opportunity to meet and form new friendships in a safe and inclusive
+						setting. We strive to promote the love of golf in a competitive
+						environment.
+					</p>
+					<p className='mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl'>
+						Golf & Membership Group
+					</p>
 
 					<div className='mt-10 sm:flex sm:justify-center lg:justify-start'>
 						<div className='rounded-md shadow'>
