90aa116200ea6851b9cb75f8343fd694a747fc42
Kellen Wiltshire <kellenwiltshire@outlook.com>
2021-11-15 09:05:56 -0500

    Added Mission Statement

--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -38,9 +38,9 @@ export default function Hero() {
 					</div>
 				</div>
 			</div>
-			<div className='relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-2 lg:right-0 lg:w-1/2 lg:h-full'>
+			<div className='relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-2 lg:right-0 lg:w-1/2 lg:h-96'>
 				<Image
-					className='w-full h-full object-cover'
+					className='object-cover'
 					src='/brand/logoWithText.jpg'
 					height={868}
 					width={587}
