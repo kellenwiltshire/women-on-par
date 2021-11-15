03af924fe612bf1505b3c555546c69ea6ee5f744
Kellen Wiltshire <kellenwiltshire@outlook.com>
2021-11-15 15:35:08 -0500

    Added Progress Bar

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,12 @@
 import 'tailwindcss/tailwind.css';
 import Layout from '@/components/layout/Layout.jsx';
+import nProgress from 'nprogress';
+import { Router } from 'next/router';
+import 'nprogress/nprogress.css';
+
+Router.events.on('routeChangeStart', nProgress.start);
+Router.events.on('routeChangeError', nProgress.done);
+Router.events.on('routeChangeComplete', nProgress.done);
 
 function MyApp({ Component, pageProps }) {
 	return (
