fa732cefc093a2bc154ad8db49f97591d2e11384
Kellen Wiltshire <kellenwiltshire@outlook.com>
2021-12-02 10:39:06 -0500

    fix: picture sizing on News page

--- components/News/News.jsx
+++ components/News/News.jsx
@@ -13,7 +13,7 @@ export default function Articles({ news }) {
 	return (
 		<div className='bg-white overflow-hidden'>
 			<div className='relative max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8'>
-				<div className='hidden lg:block bg-gray-50 absolute top-0 bottom-0 left-3/4 w-screen' />
+				{/* <div className='hidden lg:block bg-gray-50 absolute top-0 bottom-0 left-3/4 w-screen' /> */}
 				<div className='mx-auto text-base max-w-prose lg:grid lg:grid-cols-2 lg:gap-8 lg:max-w-none'>
 					<div>
 						<h3 className='mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl'>
@@ -23,7 +23,7 @@ export default function Articles({ news }) {
 				</div>
 				<div className='mt-8 lg:grid lg:grid-cols-2 lg:gap-8'>
 					<div className='relative lg:row-start-1 lg:col-start-2'>
-						<svg
+						{/* <svg
 							className='hidden lg:block absolute top-0 right-0 -mt-20 -mr-20'
 							width={404}
 							height={384}
@@ -44,17 +44,11 @@ export default function Articles({ news }) {
 								</pattern>
 							</defs>
 							<rect width={404} height={384} fill='url(#de316486-4a29-4312-bdfc-fbce2132a2c1)' />
-						</svg>
+						</svg> */}
 						<div className='relative text-base mx-auto max-w-prose lg:max-w-none'>
 							<figure>
 								<div className='aspect-w-12 aspect-h-7 lg:aspect-none'>
-									<Image
-										className='rounded-lg shadow-lg object-cover object-center'
-										src={image}
-										alt=''
-										width={1184}
-										height={1376}
-									/>
+									<img className='rounded-lg shadow-lg' src={image} alt='' />
 								</div>
 							</figure>
 						</div>
