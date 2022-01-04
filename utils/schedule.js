53b274df0c84f14db4fa87387d3f4d26a9d85014
Kellen Wiltshire <kellenwiltshire@outlook.com>
2022-01-04 07:32:51 -0500

    working on scheduler

---
+++ utils/schedule.js
@@ -0,0 +1,13 @@
+/* 
+
+Info needed for Scheduler:
+
+-Golfer - Name, Tee Time Restriction, Car Pool
+-Date and Tee Time
+-Course - Interval
+
+*/
+
+export default function generateSchedule(golfers, schedule, course) {
+	return teeTimes;
+}
