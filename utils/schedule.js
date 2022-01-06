ea60ed594e97713442e9c8754eb966fc5307e463
Kellen Wiltshire <kellenwiltshire@outlook.com>
2022-01-06 17:01:42 -0500

    feat: integrating new scheduler

--- utils/schedule.js
+++ utils/schedule.js
@@ -1,26 +1,3 @@
-/* 
-
-Info needed for Scheduler:
-
--Golfer - Name, Tee Time Restriction, Car Pool
--Date and Tee Time
--Course - Interval
-
-Probable Steps: 
-
-1 - Randomize list of golfers - Doing this to make sure the list is different every time and not based on Alphabetical or "join" order
-2 - Take this list and start iterating through it putting golfers into Tee Time Objects to go into Array
-3 - At each Golfer check to see if they have a Tee Time Restriction or Car Pool Condition - If no restrictions, then golfer added to Tee Time. Once Tee Time gets 4 golfers, then that Tee Time is placed into Final Array and Tee Time is incremented by the Course interval and a new Object is created
-4 - If Golfer has a Tee Time Restriction, put that golfer at a random new position in Golfer Array. If the Golfer has a Car Pool Condition, find that person in the array and move their position to be Curr Golfer position plus random number between 1 and 10 to make sure that they are close enough for waiting time
-5 - Continue until all Golfers are put into Tee Time Object
-
-Layout of Final Array Objects:
-Tee Time: TIME
-golfers: [golfer1, 2, 3, 4]
-
-*/
-
-//Need to expand this to 48 (max number of golfers)
 const golfers = [
 	{ name: 'Player One', carpool: '', teeTime: false },
 	{ name: 'Player Two', carpool: 'Player Nine', teeTime: false },
@@ -79,7 +56,7 @@ const golfers = [
 
 const schedule = { startTime: '16:00:00' };
 
-const course = { interval: `00:07:00`, maxSlots: 12 };
+const course = { interval: 7, timeslots: 12 };
 
 const randomizeGolfers = (golfers) => {
 	let currentIndex = golfers.length,
@@ -153,7 +130,6 @@ const testTime = (time) => {
 
 	let hours = times[0];
 	let minutes = times[1];
-	let seconds = times[2];
 
 	if (hours >= 16 && minutes >= 30) {
 		return true;
@@ -173,12 +149,12 @@ generateSchedule(golfers, schedule, course);
 
 function generateSchedule(golfers, schedule, course) {
 	//Deterime Max Golfers
-	const maxGolfers = course.maxSlots * 4;
+	const maxGolfers = course.timeslots * 4;
 
 	//Create initial Variables
 	const initialGolfers = golfers;
 	const initialStartTime = schedule.startTime;
-	const interval = course.interval;
+	const interval = '00:' + ('0' + course.interval).slice(-2) + ':00';
 
 	//Create the Waiting List and Usuable Golfers Array
 	let waitingList = [];
