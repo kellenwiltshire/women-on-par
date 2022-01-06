ac6f048e5f24509a98143d3a6466313323b7104e
Kellen Wiltshire <kellenwiltshire@outlook.com>
2022-01-06 16:58:54 -0500

    feat: integrating new scheduler

--- utils/schedule.js
+++ utils/schedule.js
@@ -16,19 +16,17 @@ Probable Steps:
 
 Layout of Final Array Objects:
 Tee Time: TIME
-GolferOne: NAME
-GolferTwo: NAME
-GolferThree: NAME
-GolferFour: NAME
+golfers: [golfer1, 2, 3, 4]
 
 */
 
+//Need to expand this to 48 (max number of golfers)
 const golfers = [
 	{ name: 'Player One', carpool: '', teeTime: false },
 	{ name: 'Player Two', carpool: 'Player Nine', teeTime: false },
 	{ name: 'Player Three', carpool: '', teeTime: false },
 	{ name: 'Player Four', carpool: '', teeTime: true },
-	{ name: 'Player Five', carpool: 'Player Five', teeTime: false },
+	{ name: 'Player Five', carpool: 'Player Thirteen', teeTime: false },
 	{ name: 'Player Six', carpool: '', teeTime: false },
 	{ name: 'Player Seven', carpool: '', teeTime: false },
 	{ name: 'Player Eight', carpool: '', teeTime: false },
@@ -38,13 +36,50 @@ const golfers = [
 	{ name: 'Player Twelve', carpool: '', teeTime: false },
 	{ name: 'Player Thirteen', carpool: 'Player Five', teeTime: false },
 	{ name: 'Player Fourteen', carpool: '', teeTime: false },
+	{ name: 'Player Fifteen', carpool: '', teeTime: false },
+	{ name: 'Player Sixteen', carpool: '', teeTime: false },
+	{ name: 'Player Seventeen', carpool: '', teeTime: false },
+	{ name: 'Player Eighteen', carpool: '', teeTime: true },
+	{ name: 'Player Nineteen', carpool: '', teeTime: false },
+	{ name: 'Player Twenty', carpool: '', teeTime: false },
+	{ name: 'Player Twentyone', carpool: '', teeTime: false },
+	{ name: 'Player Twentytwo', carpool: '', teeTime: false },
+	{ name: 'Player Twentythree', carpool: '', teeTime: false },
+	{ name: 'Player TwentyFour', carpool: '', teeTime: true },
+	{ name: 'Player TwentyFive', carpool: '', teeTime: false },
+	{ name: 'Player TwentySix', carpool: '', teeTime: false },
+	{ name: 'Player TwentySeven', carpool: '', teeTime: false },
+	{ name: 'Player TwentyEight', carpool: '', teeTime: false },
+	{ name: 'Player TwentyNine', carpool: '', teeTime: false },
+	{ name: 'Player Thirty', carpool: '', teeTime: false },
+	{ name: 'Player ThirtyOne', carpool: '', teeTime: false },
+	{ name: 'Player ThirtyTwo', carpool: '', teeTime: true },
+	{ name: 'Player ThirtyThree', carpool: '', teeTime: false },
+	{ name: 'Player ThirtyFour', carpool: '', teeTime: false },
+	{ name: 'Player ThirtyFive', carpool: '', teeTime: false },
+	{ name: 'Player ThirtySix', carpool: '', teeTime: false },
+	{ name: 'Player ThirtySeven', carpool: '', teeTime: false },
+	{ name: 'Player ThirtyEight', carpool: '', teeTime: true },
+	{ name: 'Player ThirtyNine', carpool: '', teeTime: false },
+	{ name: 'Player Forty', carpool: '', teeTime: false },
+	{ name: 'Player FortyOne', carpool: '', teeTime: false },
+	{ name: 'Player FortyTwo', carpool: '', teeTime: false },
+	{ name: 'Player FortyThree', carpool: '', teeTime: false },
+	{ name: 'Player FortyFour', carpool: '', teeTime: false },
+	{ name: 'Player FortyFive', carpool: '', teeTime: false },
+	{ name: 'Player FortySix', carpool: '', teeTime: true },
+	{ name: 'Player FortySeven', carpool: '', teeTime: false },
+	{ name: 'Player FortyEight', carpool: '', teeTime: false },
+	{ name: 'Player FORTYNINE', carpool: '', teeTime: false },
+	{ name: 'Player FIFTY', carpool: '', teeTime: false },
+	{ name: 'Player FIFTYONE', carpool: '', teeTime: false },
+	{ name: 'Player FIFTYTWO', carpool: '', teeTime: true },
+	{ name: 'Player FIFTYTHREE', carpool: '', teeTime: false },
 ];
 
-const schedule = { startTime: '16:00' };
+const schedule = { startTime: '16:00:00' };
 
-const course = { interval: 7 };
-
-generateSchedule(golfers, schedule, course);
+const course = { interval: `00:07:00`, maxSlots: 12 };
 
 const randomizeGolfers = (golfers) => {
 	let currentIndex = golfers.length,
@@ -57,51 +92,205 @@ const randomizeGolfers = (golfers) => {
 		currentIndex--;
 
 		//and swap it with the current element
-		[golfers[currentIndex], golfers[randomIndex]] = [
-			golfers[randomIndex],
-			golfers[currentIndex],
-		];
+		[golfers[currentIndex], golfers[randomIndex]] = [golfers[randomIndex], golfers[currentIndex]];
 	}
 
 	return golfers;
 };
 
-export default function generateSchedule(golfers, schedule, course) {
-	const golfersArray = randomizeGolfers(golfers);
+const addTimeInterval = (currTime, interval) => {
+	let times = [0, 0, 0];
+	let max = times.length;
 
-	let teeTimes = [];
-	let currTime = schedule.startTime;
-	let interval = course.interval;
-	let group = {teeTime: currTime};
-    let groupNum = 0;
+	let a = currTime.split(':');
+	let b = interval.split(':');
 
-	for(let i = 0; i < golfersArray.length; i++){
-        if(!golfersArray[i].teeTime){
-            if(groupNum === 0){
-                group.golferOne = golfersArray[i];
-                golfersArray.splice(i, 1);
-                groupNum++;
-            } else if (groupNum === 1){
-                group.golferTwo = golfersArray[i];
-                golfersArray.splice(i, 1);
-                groupNum++;
-            } else if (groupNum === 2){
-                group.golferThree = golfersArray[i];
-                golfersArray.splice(i, 1);
-                groupNum++;
-            } else if (groupNum === 4){
-                group.golferFour = golfersArray[i];
-                golfersArray.splice(i, 1);
-                teeTimes.push(group);
-                group = {};
-                currTime += interval;
-                group.teeTime = currTime;
-                groupNum = 0;
-            }
-        }
-    }
+	//normalize values
+	for (let i = 0; i < max; i++) {
+		a[i] = isNaN(parseInt(a[i])) ? 0 : parseInt(a[i]);
+		b[i] = isNaN(parseInt(b[i])) ? 0 : parseInt(b[i]);
+	}
 
-    console.log(teeTimes;);
+	//store values
+	for (let i = 0; i < max; i++) {
+		times[i] = a[i] + b[i];
+	}
 
-	return teeTimes;
+	let hours = times[0];
+	let minutes = times[1];
+	let seconds = times[2];
+
+	if (seconds >= 60) {
+		let m = (seconds / 60) << 0;
+		minutes += m;
+		seconds -= 60 * m;
+	}
+
+	if (minutes >= 60) {
+		let h = (minutes / 60) << 0;
+		hours += h;
+		minutes -= 60 * h;
+	}
+
+	return ('0' + hours).slice(-2) + ':' + ('0' + minutes).slice(-2) + ':' + ('0' + seconds).slice(-2);
+};
+
+const testTime = (time) => {
+	let times = [0, 0, 0];
+	let max = times.length;
+
+	let a = time.split(':');
+
+	//normalize values
+	for (let i = 0; i < max; i++) {
+		a[i] = isNaN(parseInt(a[i])) ? 0 : parseInt(a[i]);
+	}
+
+	//store values
+	for (let i = 0; i < max; i++) {
+		times[i] = a[i];
+	}
+
+	let hours = times[0];
+	let minutes = times[1];
+	let seconds = times[2];
+
+	if (hours >= 16 && minutes >= 30) {
+		return true;
+	} else {
+		return false;
+	}
+};
+
+const filterArray = (arr1, arr2) => {
+	const filtered = arr1.filter((el) => {
+		return arr2.indexOf(el) === -1;
+	});
+	return filtered;
+};
+
+generateSchedule(golfers, schedule, course);
+
+function generateSchedule(golfers, schedule, course) {
+	//Deterime Max Golfers
+	const maxGolfers = course.maxSlots * 4;
+
+	//Create initial Variables
+	const initialGolfers = golfers;
+	const initialStartTime = schedule.startTime;
+	const interval = course.interval;
+
+	//Create the Waiting List and Usuable Golfers Array
+	let waitingList = [];
+	let usableGolfers = [];
+
+	//Create Final Array to be filled
+	let finalTeeTimeArray = [];
+
+	//More initial variables to be filled and changed
+	let currTime = initialStartTime;
+	let group = { teeTime: currTime, golfers: [] };
+	let groupNum = 0;
+
+	//Trim the list of golfers if there is more than the max number of golfers
+	if (initialGolfers.length > maxGolfers) {
+		while (initialGolfers.length > maxGolfers) {
+			waitingList.push(initialGolfers[golfers.length - 1]);
+			initialGolfers.pop();
+		}
+		usableGolfers = randomizeGolfers(golfers);
+	} else {
+		usableGolfers = randomizeGolfers(golfers);
+	}
+
+	//Make a first pass of the golfers to filter out those with a tee time restriction
+	let teeTimeRestrictions = [];
+	let unrestrictedGolfers = [];
+	usableGolfers.forEach((golfer) => {
+		golfer.teeTime ? teeTimeRestrictions.push(golfer) : unrestrictedGolfers.push(golfer);
+	});
+
+	//Start filling a new Golfer Array with unrestricted golfers until it passes the time restriction
+	let timeTestPassed = false;
+	let newGolferArray = [];
+	unrestrictedGolfers.forEach((golfer, i) => {
+		if (!timeTestPassed) {
+			if (groupNum < 3) {
+				console.log('Added to Array');
+				newGolferArray.push(golfer);
+				groupNum++;
+			} else if (groupNum === 3) {
+				console.log('Added to Array');
+				newGolferArray.push(golfer);
+				currTime = addTimeInterval(currTime, interval);
+				console.log(currTime);
+				const timeTest = testTime(currTime);
+				if (timeTest) {
+					timeTestPassed = true;
+					console.log('It is Passed 4:30');
+					//Once it is passed the time restriction of 4:30 combine the rest of the unrestricted golfers and restricted golfers, then randomize. Then add this one the end of the already scheduled golfers
+					const combineArray = teeTimeRestrictions.concat(unrestrictedGolfers);
+					console.log('Combine Array: ', combineArray);
+					const filteredArray = filterArray(combineArray, newGolferArray);
+					const randomFilteredArray = randomizeGolfers(filteredArray);
+					newGolferArray = newGolferArray.concat(randomFilteredArray);
+				} else {
+					groupNum = 0;
+					console.log('reset');
+				}
+			}
+		}
+	});
+
+	//Reset the variables
+	groupNum = 0;
+	currTime = schedule.startTime;
+
+	//Before setting final tee times will need to iterate through array and look for car pooling people. Once one is found, will need to find the matching person and splice them out of their current position (unless within 1-12 golfers away from original person) and splice them back in at a random interval (1-12) from original golfer to make sure they are within 3 tee times of eachother
+
+	for (let i = 0; i < newGolferArray.length; i++) {
+		let name = '';
+		if (newGolferArray[i].carpool) {
+			name = newGolferArray[i].carpool;
+			const golferIndex = newGolferArray.findIndex((obj) => {
+				if (obj.name === name) {
+					return true;
+				}
+
+				return false;
+			});
+
+			if (golferIndex > i) {
+				if (golferIndex > i + 11) {
+					const newPosition = Math.floor(Math.random() * 11);
+					const golferMoving = newGolferArray[golferIndex];
+					newGolferArray.splice(golferIndex, 1);
+					newGolferArray.splice(newPosition + i, 0, golferMoving);
+				}
+			}
+		}
+	}
+
+	console.log('New Golfer Array: ', newGolferArray);
+
+	//Finally iterate through the golfer array and place them in their tee time slots.
+	newGolferArray.forEach((golfer, i) => {
+		if (groupNum < 3) {
+			group.golfers.push(golfer);
+			groupNum++;
+			if (i === newGolferArray.length - 1) {
+				finalTeeTimeArray.push(group);
+			}
+		} else {
+			group.golfers.push(golfer);
+			currTime = addTimeInterval(currTime, interval);
+			finalTeeTimeArray.push(group);
+			group = { teeTime: currTime, golfers: [] };
+			groupNum = 0;
+		}
+	});
+
+	console.log('Tee times: ', finalTeeTimeArray);
+
+	// return teeTimes;
 }
