87344dc540f4de343acd3d68840cc76324649f20
Kellen <webdevelopment@kellenwiltshire.com>
2022-05-02 11:23:03 -0400

    feat: working on editing of tee time schedule

---
+++ components/Buttons/UpButton.tsx
@@ -0,0 +1,15 @@
+import { ArrowSmUpIcon } from '@heroicons/react/solid';
+import React from 'react';
+
+function UpButton() {
+	return (
+		<button
+			type='button'
+			className='inline-flex items-center border border-transparent rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+		>
+			<ArrowSmUpIcon className='h-5 w-5' aria-hidden='true' />
+		</button>
+	);
+}
+
+export default UpButton;
--- components/TeeTimeGenerater/ScheduleCards.tsx
+++ components/TeeTimeGenerater/ScheduleCards.tsx
@@ -1,12 +1,48 @@
 import React from 'react';
+import DownButton from '../Buttons/DownButton';
+import UpButton from '../Buttons/UpButton';
 
 export default function ScheduleCards({ schedule, waitingList }): JSX.Element {
 	const newWaitingList = waitingList.reverse();
+
+	const handleMoveDown = (golfer) => {
+		const findTeetime = schedule.find((teeTime) =>
+			teeTime.golfers.find((player) => player.id === golfer.id),
+		);
+
+		const findTeetimeIndex = schedule.findIndex(
+			(teeTime) => teeTime.teeTime === findTeetime.teeTime,
+		);
+
+		const findPlayerIndex = findTeetime.golfers.findIndex(
+			(player) => player.id === golfer.id,
+		);
+
+		if (findPlayerIndex >= findTeetime.golfers.length) {
+			const item1 = schedule[findTeetimeIndex + 1].golfers[0];
+			const item2 = findTeetime.golfers[findPlayerIndex];
+
+			schedule[findTeetimeIndex + 1].golfers.splice(0, 1);
+			schedule[findTeetimeIndex + 1].golfers.splice(0, 0, item2);
+
+			schedule[findTeetimeIndex + 1].golfers.splice(findPlayerIndex, 1);
+			schedule[findTeetimeIndex + 1].golfers.splice(findPlayerIndex, 0, item1);
+		} else {
+			const item = findTeetime.golfers[findPlayerIndex];
+			findTeetime.golfers.splice(findPlayerIndex, 1);
+			findTeetime.golfers.splice(findPlayerIndex + 1, 0, item);
+		}
+
+		console.log(findTeetime.golfers);
+	};
 	return (
 		<div className='flex w-full justify-center flex-row flex-wrap'>
 			{schedule.map((teeTime) => {
 				return (
-					<div key={teeTime.teeTime} className='flex shadow-sm rounded-md m-2 w-1/4'>
+					<div
+						key={teeTime.teeTime}
+						className='flex shadow-sm rounded-md m-2 w-1/4'
+					>
 						<div className='flex-shrink-0 flex flex-col items-center justify-center w-16 text-black text-sm font-medium rounded-l-md border'>
 							<p>{teeTime.teeTime}</p>
 						</div>
@@ -15,7 +51,11 @@ export default function ScheduleCards({ schedule, waitingList }): JSX.Element {
 								return (
 									<div key={golfer.id}>
 										<p className='mx-1'>
-											{golfer.first_name} {golfer.last_name}
+											{golfer.first_name} {golfer.last_name} <UpButton />{' '}
+											<DownButton
+												handleClick={handleMoveDown}
+												golfer={golfer}
+											/>
 										</p>
 										{/* {golfer.carpool ? <p className='mx-1 text-xs text-gray-400'>carpool: {golfer.carpool}</p> : null} */}
 									</div>
