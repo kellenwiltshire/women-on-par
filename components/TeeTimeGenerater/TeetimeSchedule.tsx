85ee31bf77bcdf9fba16e33addb3865ea87e0893
Kellen Wiltshire <kellenwiltshire@outlook.com>
2022-05-04 08:06:02 -0400

    chore: refactor

--- components/TeeTimeGenerater/TeetimeSchedule.tsx
+++ components/TeeTimeGenerater/TeetimeSchedule.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { useReactToPrint } from 'react-to-print';
 import CloseSchedule from '../Modals/CloseSchedule';
 import ScheduleCards from './ScheduleCards';
