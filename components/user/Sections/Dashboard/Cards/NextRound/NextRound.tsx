7a0675e28426d5ad0305385fea82c8ca18cc87fc
Kellen Wiltshire <kellenwiltshire@outlook.com>
2022-06-03 07:45:28 -0400

    fix: reworking Next Round Info

--- components/user/Sections/Dashboard/Cards/NextRound/NextRound.tsx
+++ components/user/Sections/Dashboard/Cards/NextRound/NextRound.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { CalendarIcon } from '@heroicons/react/outline';
 import { findNextRound } from '@/utils/sortingFunctions';
 import NextRoundInfo from './NextRoundInfo';
@@ -7,14 +7,55 @@ import { useScheduleContext, useUserContext } from '@/context/Store';
 import SaveSuccess from '@/components/Notifications/SaveSuccess';
 import SaveFail from '@/components/Notifications/SaveFail';
 
+function findCurrentRound(schedules) {
+	const currDate = new Date();
+
+	if (schedules.length) {
+		const nextRound = schedules.filter((round) => {
+			const date = new Date(round.date);
+			if (date === currDate) {
+				return round;
+			}
+		});
+
+		return nextRound;
+	} else {
+		const nextRound = {};
+		return nextRound;
+	}
+}
+
 export default function NextRound(): JSX.Element {
 	const user = useUserContext();
 	const schedule = useScheduleContext();
+	const currDate = new Date();
 
 	const [success, setSuccess] = useState(false);
 	const [failure, setFailure] = useState(false);
 
-	const nextRound = findNextRound(schedule);
+	const [nextRound, setNextRound] = useState(findNextRound(schedule));
+
+	const [cutOffPast, setCutOffPast] = useState(false);
+
+	useEffect(() => {
+		const dayOfWeek = currDate.getDay(); //0 is Sunday
+
+		if (dayOfWeek >= 1 && dayOfWeek <= 3) {
+			if (dayOfWeek === 3) {
+				const time = currDate.getHours();
+
+				if (time > 14) {
+					setCutOffPast(false);
+				} else {
+					setNextRound(findCurrentRound(schedule));
+					setCutOffPast(true);
+				}
+			} else {
+				setCutOffPast(true);
+			}
+		}
+	}, []);
+
 	return (
 		<div className='rounded-tl-lg rounded-tr-lg sm:rounded-tr-none relative group bg-white p-6'>
 			{success ? <SaveSuccess show={success} setShow={setSuccess} /> : null}
@@ -23,7 +64,7 @@ export default function NextRound(): JSX.Element {
 				<CalendarIcon className='h-6 w-6' aria-hidden='true' />
 			</div>
 			<NextRoundInfo nextRound={nextRound} />
-			<NextRoundForm user={user} setSuccess={setSuccess} setFailure={setFailure} />
+			<NextRoundForm user={user} setSuccess={setSuccess} setFailure={setFailure} cutOffPast={cutOffPast} />
 		</div>
 	);
 }
