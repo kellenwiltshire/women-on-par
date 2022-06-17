432ce8b82ab7ccceb1bbd68f34ae2b6afd57c953
Kellen <webdevelopment@kellenwiltshire.com>
2022-06-17 08:41:49 -0400

    feat: add new API endpoints

---
+++ pages/api/getSchedule.ts
@@ -0,0 +1,42 @@
+import Cors from 'cors';
+import { NextApiRequest, NextApiResponse } from 'next';
+const { parseCookies } = require('nookies');
+
+const cors = Cors({
+	methods: ['GET', 'HEAD'],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+	return new Promise((resolve, reject) => {
+		fn(req, res, (result) => {
+			if (result instanceof Error) {
+				return reject(result);
+			}
+
+			return resolve(result);
+		});
+	});
+}
+
+const getSchedule = async (req: NextApiRequest, res: NextApiResponse) => {
+	await runMiddleware(req, res, cors);
+	const url = process.env.DATABASE_URL;
+
+	const cookies = parseCookies({ req });
+	const jwt = cookies.jwt;
+
+	const request = await fetch(`${url}/schedules`, {
+		method: 'GET',
+		headers: {
+			Authorization: `Bearer ${jwt}`,
+		},
+	});
+
+	const response = await request.json();
+
+	res.json(response);
+};
+
+export default getSchedule;
